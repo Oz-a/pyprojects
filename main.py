03a40d51af1f3d0f58590d53fab992756a946531
Ozan Ahmet Ahmet <40339705+Oz-a@users.noreply.github.com>
2022-06-30 01:50:05 +0000

    added password generator module to main

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 from countdown import countdown
+from passgen import generate_password
+import time
+
 countdown() #count down from a user input number
+print('Now you can generate a password')
+time.sleep(1)
+generate_password()
